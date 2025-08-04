19e9ce0760e56ba6fc1f2b274efa4b592a4f5e7b
Vlad_probel <evstegnevvlad30@gmail.com>
2025-08-04 20:06:05 +0200

    Типы данных

--- PythonBese/lessen-1.py
+++ PythonBese/lessen-1.py
@@ -1,1 +1,37 @@
-print('hello')
+num = 15
+print(id(num)) # id() - выводит id
+
+# целое число /  integer / int
+my_int =  19
+print(my_int, type(my_int))
+
+# Дробные числа / вещественные числа / числа с плавающей точкой / float
+my_float = 0.19
+print(my_float, type(my_float))
+
+# строки / string / str
+my_str_1 = 'h"llo'
+my_str_2 = "he'lo"
+print(my_str_1, type(my_str_1))
+print(my_str_2, type(my_str_2))
+
+# списки / list
+my_list = [19, 91, 'hello', 0.19 ]
+print(my_list, type(my_list))
+
+# кортеж  / tuple
+my_tuple = ("popov", 'oleg', 4)
+print(my_tuple, type(my_tuple))
+
+# множество / set
+my_set = {1,2,3,2,1,2,3,21,1}
+print(my_set, type(my_set))
+
+#словарь / dictionary / dict
+my_dict = { 'neme' :'larisa',  'age' :91, 'lastneme': 'Nesterova' }
+print(my_dict, type(my_dict))
+
+# Логический тип данных / boolean / bool
+my_bool1 = True
+my_bool2 = False
+print(my_bool1, type(my_bool1))
