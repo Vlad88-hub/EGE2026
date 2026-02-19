13ee7841bc165aa05209d81280ec0d18be4dd087
Vlad_probel <evstegnevvlad30@gmail.com>
2026-02-19 20:01:25 +0200

    23 задачи (19.02)

--- Task-8/10921.py
+++ Task-8/10921.py
@@ -6,4 +6,23 @@ for val in set(permutations('ДЖ*ВАСКРИПТ')):
     if val.index('А') + 1 + val.index('*') + 1 + val.index('И') + 1 == 11:
         cnt += 1
 
+print(cnt/2)
+
+#####################################################
+
+
+from itertools import permutations
+
+cnt = 0
+for val in set(permutations('ДЖАВАСКРИПТ')):
+    val = ''.join(val)
+    summ = 0
+    for pos, buk in enumerate(val, start=1):
+        if buk in 'АИ':
+            summ += pos
+            if summ == 11:
+                cnt += 1
+
 print(cnt)
+
+
