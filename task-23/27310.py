df8f8fdcb739ef6a1fd3a25ffe917dfe5aa79183
Vlad_probel <evstegnevvlad30@gmail.com>
2026-03-26 19:44:32 +0200

    теория по 16, 23, 19-21 задачам (26.03)

---
+++ task-23/27310.py
@@ -0,0 +1,15 @@
+def f(start, end, flag):
+    if start in [30, 42]: flag = True
+    if start == end and flag: return 1
+    if start < end: return 0
+    return f(start - 3, end, flag) + f(start - 4, end, flag) + f(start // 2, end, flag)
+
+print(f(78, 2, False))
+
+def f(start, end, cnt):
+    if start in range(31, 43): cnt += 1
+    if start == end and cnt == 1: return 1
+    if start < end: return 0
+    return f(start - 3, end, cnt) + f(start - 4, end, cnt) + f(start // 2, end, cnt)
+
+print(f(78, 2, 0))
