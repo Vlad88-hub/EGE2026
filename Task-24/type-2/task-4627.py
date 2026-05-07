44071e41e843aca58f01c674b7d32eb3841aa6e7
Vlad_probel <evstegnevvlad30@gmail.com>
2026-05-07 19:49:09 +0200

    24 задания - (07.05)

--- Task-24/type-2/task-4627.py
+++ Task-24/type-2/task-4627.py
@@ -2,13 +2,17 @@ with open(r'../files/24_4627.txt') as file:
     data = file.readline()
 
 ans = 0
+checkpoint = 0
 for i in range(len(data) - 2):
+    if checkpoint > i:
+        continue
     if data[i:i+3] in ['NPO', 'PNO']:
         cnt = 1
         for j in range(i+3, len(data), 3):
             if data[j:j+3] in ['NPO', 'PNO']:
                 cnt += 1
             else:
+                checkpoint = j
                 break
         ans = max(ans, cnt)
 
--- Task-24/type-2/task-7600.py
+++ Task-24/type-2/task-7600.py
@@ -1,25 +1,26 @@
 with open(r'../files/24_7600.txt') as file:
     data = file.readline()
 
-ans = 0
-cnt = 0
-for i in range(len(data)-1):
-    if data[i:i+2] not in ['QR', 'RQ', 'QS', 'SQ', 'QQ', 'RS', 'SR', 'RS', 'RR', 'SS']:
-        cnt += 1
-    else:
-        ans = max(ans, cnt)
-        cnt = 0
-
-print(ans + 1)
-
-# cnt_1 = 0
-# ans_1 = 0
+# ans = 0
+# cnt = 0
 # for i in range(len(data)-1):
-#     if data[i] not in 'QRS' and data[i+1] not in 'QRS':
-#         cnt_1 += 1
+#     if data[i:i+2] not in ['QR', 'RQ', 'QS', 'SQ', 'QQ', 'RS', 'SR', 'RS', 'RR', 'SS']:
+#         cnt += 1
 #     else:
-#         ans_1 = max(ans_1, cnt_1)
+#         ans = max(ans, cnt)
+#         cnt = 0
 #
-# ans_1 = max(ans_1, cnt_1)
-#
-# print(ans_1)
+# print(ans + 1)
+
+cnt_1 = 0
+ans_1 = 1
+for i in range(len(data)-1):
+    if data[i] not in 'QRS' or data[i+1] not in 'QRS':
+        cnt_1 += 1
+    else:
+        ans_1 = max(ans_1, cnt_1)
+        cnt_1 = 1
+
+ans_1 = max(ans_1, cnt_1)
+
+print(ans_1)
